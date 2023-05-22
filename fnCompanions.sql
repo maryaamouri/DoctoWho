af9f34a50d817cb5ea7344662bbb1ac3c26c4586
Marya Amouri <82655833+maryaamouri@users.noreply.github.com>
2023-05-22 22:17:38 +0300

    Update fnCompanions.sql

--- fnCompanions.sql
+++ fnCompanions.sql
@@ -2,15 +2,13 @@ CREATE FUNCTION fnCompanions (@EpisodeId INT)
 RETURNS VARCHAR(MAX)
 AS
 BEGIN
-    DECLARE @Companions VARCHAR(MAX);
-    SELECT @Companions = COALESCE(@Companions + ', ', '') + c.CompanyName
-    FROM tblEpisodeCompanion e
-    INNER JOIN 
-	tblCompanion c 
-	ON c.CompanionId = e.CompanionId
-    WHERE e.EpisodeId = @EpisodeId;
-    RETURN @Companions;
-END;
+    DECLARE @CompanionList VARCHAR(MAX);
 
-SELECT dbo.fnCompanions(8) AS CompanionName;
-SELECT * FROM tblCompanion;
+    SELECT @CompanionList = COALESCE(@CompanionList + ', ', '') + c.CompanyName
+    FROM tblEpisodeCompanion ec
+    INNER JOIN tblCompanion c ON c.CompanionId = ec.CompanionId
+    WHERE ec.EpisodeId = @EpisodeId;
+
+    RETURN @CompanionList;
+END;
+SELECT dbo.fnCompanions(12) AS CompanionName;
