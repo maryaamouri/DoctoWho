900e21d6a066a5c475539c5a6d152ae91d7c7b3b
Marya Amouri <82655833+maryaamouri@users.noreply.github.com>
2023-05-23 00:09:59 +0300

    Update fnCompanions.sql

--- fnCompanions.sql
+++ fnCompanions.sql
@@ -1,14 +1,14 @@
 CREATE FUNCTION fnCompanions (@EpisodeId INT)
-RETURNS VARCHAR(MAX)
+RETURNS TABLE 
 AS
-BEGIN
-    DECLARE @CompanionList VARCHAR(MAX);
+RETURN (
+    SELECT STRING_AGG(CompanyName , ', ') AS ConcatenatedValues
+    FROM (
+        SELECT c.CompanyName 
+        FROM tblEpisodeCompanion ec INNER JOIN tblCompanion c 
+		ON c.CompanionId = ec.CompanionId
+		WHERE ec.EpisodeId = @EpisodeId) AS subquery
+);
 
-    SELECT @CompanionList = COALESCE(@CompanionList + ', ', '') + c.CompanyName
-    FROM tblEpisodeCompanion ec
-    INNER JOIN tblCompanion c ON c.CompanionId = ec.CompanionId
-    WHERE ec.EpisodeId = @EpisodeId;
-
-    RETURN @CompanionList;
-END;
-SELECT dbo.fnCompanions(12) AS CompanionName;
+SELECT *
+FROM dbo.fnCompanions(11);
