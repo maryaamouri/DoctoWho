7861d2beeea6e5385975520f3551d7d2460e79d0
Marya Amouri <82655833+maryaamouri@users.noreply.github.com>
2023-05-23 00:01:25 +0300

    Update fnEnemies.sql

--- fnEnemies.sql
+++ fnEnemies.sql
@@ -2,12 +2,15 @@ CREATE FUNCTION fnEnemies (@EpisodeId INT)
 RETURNS TABLE 
 AS
 RETURN (
-SELECT en.EnemyName 
-FROM tblEpisode e INNER JOIN tblEpisodeEnemy ee 
-ON e.EpisodeId = ee.EpisodeId
-INNER JOIN tblEnemy en ON ee.EnemyId=en.EnemyId
-WHERE ee.EpisodeId = @EpisodeId
+    SELECT STRING_AGG(EnemyName , ', ') AS ConcatenatedValues
+    FROM (
+        SELECT en.EnemyName 
+        FROM tblEpisode e
+        INNER JOIN tblEpisodeEnemy ee ON e.EpisodeId = ee.EpisodeId
+        INNER JOIN tblEnemy en ON ee.EnemyId = en.EnemyId
+        WHERE ee.EpisodeId = @EpisodeId
+    ) AS subquery
 );
 
-SELECT STRING_AGG(EnemyName, ', ') AS ConcatenatedValues
-FROM fnEnemies(11);
+SELECT ConcatenatedValues
+FROM dbo.fnEnemies(11);
